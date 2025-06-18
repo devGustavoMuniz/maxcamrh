fbcee87cb308130d8bdfdcda9ce04d610caaddb3
Muniz <gustavomuniz545@gmail.com>
2025-06-18 17:59:58 -0300

    fix: corrigir query pra funcionar em mysql/postgress

--- app/Http/Controllers/CollaboratorController.php
+++ app/Http/Controllers/CollaboratorController.php
@@ -102,8 +102,9 @@ class CollaboratorController extends Controller
       $searchTerm = $request->input('search');
       $query->where(function ($q) use ($searchTerm) {
         $q->whereHas('user', function ($uq) use ($searchTerm) {
-          $uq->where('name', 'like', "%{$searchTerm}%")
-            ->orWhere('email', 'like', "%{$searchTerm}%");
+          $lowerSearchTerm = strtolower($searchTerm);
+          $uq->where('name', 'like', "%{$lowerSearchTerm}%")
+            ->orWhere('email', 'like', "%{$lowerSearchTerm}%");
         });
       });
     }
