91045ea9535b0b2bd4d6a8af766f509e7ad67666
Gustavo Muniz <gustavomuniz545@gmail.com>
2025-06-26 23:26:21 -0300

    feat: seeder não roda mais em prod

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,34 +3,32 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Franchise;
-use App\Models\Client;
-use App\Models\Collaborator;
-use App\Models\Address; // Se for criar endereços avulsos, mas vamos integrar
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
-  public function run(): void
-  {
-    User::factory()->admin()->create([
-      'name' => 'Admin Principal',
-      'email' => 'admin@maxcamrh.com',
-      'password' => 'password',
-      'role' => 'admin'
-    ]);
+    public function run(): void
+    {
+        User::factory()->admin()->create([
+            'name' => 'Admin Principal',
+            'email' => 'admin@maxcamrh.com',
+            'password' => 'password',
+            'role' => 'admin'
+        ]);
 
-    $franchises = User::factory()->franchiseUser()->count(5)->create()->map(function ($user) {
-      return $user->franchise; // Pegar a instância de Franchise
-    });
+        if (!App::environment('production')) {
+            $franchises = User::factory()->franchiseUser()->count(5)->create()->map(function ($user) {
+                return $user->franchise;
+            });
 
-    foreach ($franchises as $franchise) {
-      User::factory()->clientUser($franchise)->count(rand(3, 10))->create()->map(function($user){
-        return $user->client;
-      })->each(function ($client) {
-        User::factory()->collaboratorUser($client)->count(rand(5, 15))->create();
-      });
+            foreach ($franchises as $franchise) {
+                User::factory()->clientUser($franchise)->count(rand(3, 10))->create()->map(function ($user) {
+                    return $user->client;
+                })->each(function ($client) {
+                    User::factory()->collaboratorUser($client)->count(rand(5, 15))->create();
+                });
+            }
+        }
     }
-  }
 }
